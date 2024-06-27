24c2d076b2805aacf4a49bda43d2c8ec0efc47fb
Saad Aslam <saadaslam13@Gmail.com>
2024-06-27 12:48:19 -0500

    added per-user data isolation

--- pages/index.tsx
+++ pages/index.tsx
@@ -35,7 +35,7 @@ export default function App() {
     <Authenticator>
     {({ signOut, user }) => (
         <main>
-      <h1>My todos updated</h1>
+      <h1>My todos</h1>
       <h1>{user?.signInDetails?.loginId}'s todos</h1>
       <button onClick={createTodo}>+ new</button>
       <ul>
